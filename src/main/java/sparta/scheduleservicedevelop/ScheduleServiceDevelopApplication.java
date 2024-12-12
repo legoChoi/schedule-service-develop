0e1c1f156286b3ca77061a8fe41fa6ff0ffbf80a
legoChoi <chlx159@gmail.com>
2024-12-12 13:42:09 +0900

    Initial commit

---
+++ src/main/java/sparta/scheduleservicedevelop/ScheduleServiceDevelopApplication.java
@@ -0,0 +1,13 @@
+package sparta.scheduleservicedevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleServiceDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleServiceDevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sparta/scheduleservicedevelop/ScheduleServiceDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.scheduleservicedevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleServiceDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
