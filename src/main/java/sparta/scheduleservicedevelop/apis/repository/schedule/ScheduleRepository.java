729be36b6004926d551bb836d3f26af154a97259
legoChoi <chlx159@gmail.com>
2024-12-19 16:26:06 +0900

    Refactor/ 페이지네이션 group by 변경

--- src/main/java/sparta/scheduleservicedevelop/apis/repository/schedule/ScheduleRepository.java
+++ src/main/java/sparta/scheduleservicedevelop/apis/repository/schedule/ScheduleRepository.java
@@ -9,9 +9,13 @@ import sparta.scheduleservicedevelop.entity.Schedule;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom {
 
-    @Query(value = "SELECT new sparta.scheduleservicedevelop.apis.controller.schedule.dto.response.FetchScheduleListJoinResDto(" +
+    @Query(value =
+            "SELECT new sparta.scheduleservicedevelop.apis.controller.schedule.dto.response.FetchScheduleListJoinResDto(" +
             "s.title, s.contents, u.userName, COUNT(c), s.createdAt, s.updatedAt) " +
-            "FROM Schedule s INNER JOIN s.user u LEFT JOIN s.commentList c GROUP BY s",
+            "FROM Schedule s " +
+            "INNER JOIN s.user u " +
+            "LEFT JOIN s.commentList c " +
+            "GROUP BY s.title, s.contents, u.userName, s.createdAt, s.updatedAt",
     countQuery = "SELECT COUNT(s) FROM Schedule s")
     Page<FetchScheduleListJoinResDto> findScheduleAllCountBy(Pageable pageable);
 }
